a7848f6adb2bd8a681002202ba36c13089624d58
Vivek Sharma <viveksharma02005@gmail.com>
2025-01-03 17:44:43 +0530

    all set source code

--- src/app.js
+++ src/app.js
@@ -15,9 +15,9 @@ app.use(express.static("public"))
 
 //routes import
 
-import userRouter  from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js"
 
 //routes declaration  
-app.use("api/v1/users",userRouter)
+app.use("/api/v1/users",userRouter)
 
 export{ app }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import  {Router} from "express"
 import {registerUser} from "../controllers/user.controller.js"
 
 const router = Router()
+
 router.route("/register").post(registerUser)
 
 export default router
