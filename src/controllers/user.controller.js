8b8d4251c31a81f1ce291417c3bdf4bdec29cebe
Vivek Sharma <viveksharma02005@gmail.com>
2025-01-09 23:21:59 +0530

    feat: enhance user registration with avatar and cover image upload to Cloudinary

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,9 @@
 import {asyncHandler}  from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import{ user} from "../models/user.model.js"
+import{uploadOnCloudinary} from "../utils/cloudinary.js"
+import{ApiResponse} from "../utils/ApiResponse.js"
+
 
 const registerUser =  asyncHandler(async (req, res) => {
   //steps to register user
@@ -45,6 +48,43 @@ if(existedUser){
 
 const avatarLocalPath = req.files?.avatar[0]?.path;
 const coverImageLocalPath = req.files?.coverImage[0]?.path;
+ 
+//step4
+if (!avatarLocalPath) {
+  throw new  ApiError(404,"Avatar  file isrequired")
+  
+}
+
+//step5
+const avatar = await uploadOnCloudinary(avatarLocalPath)
+const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+if (!avatar) {
+  throw new ApiError(400, "Avatar file is required")
+  
+}
+
+//step6
+const user = await User.create({
+  fullName,
+  avatar: avatar.url,
+  coverImage: coverImage?.url || "",
+  email,
+  password,
+  username:username.toLowerCase()
+})
+//user checkby  id (user  created or not successfully)
+//.select k aander vo fields jo hume nhi select krni minus laga kr likh sakte h or space dekr next field
+const createdUser = await User.findById(user._id).select(
+  "-password -refreshToken"
+)
+if (!createdUser) {
+  throw new ApiError(500, "Something went wrong while registring the user ")
+}
+
+return res.status(201).json(
+  new ApiResponse(200, createdUser , "user register successfully")
+)
 
 })
     
