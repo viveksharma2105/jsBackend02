932dc0baf532b77145d09aeb8bfee0fe41cedbe8
Vivek Sharma <viveksharma02005@gmail.com>
2025-01-12 20:25:33 +0530

    register controller bugs fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -47,8 +47,13 @@ if (existedUser) {
 }
 //console.log(req.files);
 
-const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
+ const avatarLocalPath = req.files?.avatar[0]?.path;
+// const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+let coverImageLocalPath;
+if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >  0){
+  coverImageLocalPath = req.files.coverImage[0].path
+}
  
 //step4
 if (!avatarLocalPath) {
